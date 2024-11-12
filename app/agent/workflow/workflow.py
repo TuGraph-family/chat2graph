d8463cc17d5d01e771aea46de7de7d873b3dd2ed
Appointat <kuda.czk@antgroup.com>
2024-11-12 19:44:54 +0800

    feat: add Workflow class to manage a sequence of operators for execution

---
+++ app/agent/workflow/workflow.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from app.agent.workflow.operator.operator import Operator
+
+
+class Workflow:
+    """A workflow is a sequence of operators that need to be executed"""
+
+    def __init__(self):
+        self.operators: List[Operator] = None
+        self.eval_operator: Operator = None
+
+        self.input_data: str = None
+
+    def execute(self):
+        """Execute the workflow."""
+        pass
+
+    def evaluate(self):
+        """Evaluate the workflow."""
+        pass
