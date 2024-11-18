d63b21b19d6858b2a479f91d69772abece15b66e
Appointat <kuda.czk@antgroup.com>
2024-11-18 16:09:25 +0800

    feat: enhance Tool class to include args_schema for improved argument validation

--- app/toolkit/tool/tool.py
+++ app/toolkit/tool/tool.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from typing import Callable
 from uuid import uuid4
 
+from pydantic import BaseModel
+
 
 @dataclass
 class Tool(ABC):
@@ -10,7 +12,9 @@ class Tool(ABC):
 
     id: str
     function: Callable
+    args_schema: BaseModel
 
-    def __init__(self, function: Callable):
+    def __init__(self, function: Callable, args_schema: BaseModel):
         self.id = str(uuid4())
         self.function = function
+        self.args_schema = args_schema
--- app/toolkit/tool/tool_resource.py
+++ app/toolkit/tool/tool_resource.py
@@ -1,11 +1,19 @@
+from pydantic import BaseModel
+
 from app.toolkit.tool.tool import Tool
+
+
+class QuerySchema(BaseModel):
+    """The query schema."""
+
+    text: str
 
 
 class Query(Tool):
     """The query tool in the toolkit."""
 
     def __init__(self):
-        super().__init__(self.query)
+        super().__init__(self.query, args_schema=QuerySchema)
 
     def query(self, text: str) -> str:
         """Query the text."""
