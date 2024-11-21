2bf64722b434ff1a262c62802465c7383458c7a6
Appointat <kuda.czk@antgroup.com>
2024-11-21 14:33:27 +0800

    refactor: enhance Action class docstring with detailed attribute descriptions

--- app/toolkit/action/action.py
+++ app/toolkit/action/action.py
@@ -7,7 +7,15 @@ from app.toolkit.tool.tool import Tool
 
 @dataclass
 class Action(ABC):
-    """The action in the toolkit."""
+    """The action in the toolkit.
+
+    Attributes:
+        id (str): The unique identifier of the action.
+        name (str): The name of the action.
+        description (str): The description of the action.
+        next_action_names (List[str]): The names of the next actions in the toolkit.
+        tools (List[Tool]): The tools can be used in the action.
+    """
 
     id: str
     name: str
