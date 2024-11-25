dc9aa74582a15e55e40c2d75e25e2bfb364ee447
Appointat <kuda.czk@antgroup.com>
2024-11-25 22:26:46 +0800

    feat: add unique message ID and optional operation ID to AgentMessage class

--- app/memory/message.py
+++ app/memory/message.py
@@ -1,16 +1,18 @@
 from dataclasses import dataclass
-from typing import Literal
+from typing import Literal, Optional
+from uuid import uuid4
 
 
 @dataclass
 class AgentMessage:
     """Agent message"""
 
-    msg_id: str
     sender_id: str
     receiver_id: str
     status: Literal["successed", "failed", "pending", "canceled"]
     content: str
     timestamp: str
 
-    tool_log: str = None
+    msg_id: str = str(uuid4())
+    op_id: Optional[str] = None
+    tool_log: Optional[str] = None
