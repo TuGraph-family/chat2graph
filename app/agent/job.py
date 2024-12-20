00bc249559bb58cf17eadcf1c7b1486382f6a56f
Appointat <kuda.czk@antgroup.com>
2024-12-20 15:48:14 +0800

    feat(job): add JobExecutionContext dataclass and integrate it into Job class

--- app/agent/job.py
+++ app/agent/job.py
@@ -1,5 +1,17 @@
 from dataclasses import dataclass, field
+from typing import Optional
 from uuid import uuid4
+
+from app.commom.system_env import SysEnvKey, SystemEnv
+from app.memory.message import WorkflowMessage
+
+
+@dataclass
+class JobExecutionContext:
+    """JobExecutionContext is the dataclass including the context of the job."""
+
+    retry_count: int = 0
+    max_retries: int = int(SystemEnv.get(SysEnvKey.MAX_RETRIES))
 
 
 @dataclass
@@ -12,6 +24,8 @@ class Job:
         id (str): The unique identifier of the task.
         context (str): The context of the task.
         output_schema (str): The output schema of the task.
+        job_execution_context (JobExecutionContext): The context of the job execution.
+        response (Optional[WorkflowMessage]): The response of the workflow.
     """
 
     session_id: str
@@ -19,3 +33,5 @@ class Job:
     context: str = ""
     output_schema: str = "Output schema is not determined."
     id: str = field(default_factory=lambda: str(uuid4()))
+    execution_context: JobExecutionContext = field(default_factory=JobExecutionContext)
+    response: Optional[WorkflowMessage] = None
