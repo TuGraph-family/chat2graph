1171ea879ea515f4ea8951d682ff7c7f55574b20
KingSkyLi <15566300566@163.com>
2025-01-10 17:59:02 +0800

    add web source code;

---
+++ chat2graph-web/src/components/Content.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="right-content">
+      <h1></h1>
+      <!-- 这里可以添加更多的内容 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RightContent',
+  };
+  </script>
+  
+  <style scoped>
+  .right-content {
+    padding: 16px;
+    background-color: #ffffff;
+  }
+  </style>
+  
---
+++ chat2graph-web/src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="left-sidebar">
+      <h1></h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LeftSidebar',
+  };
+  </script>
+  
+  <style scoped>
+ .sidebar {
+  padding: 16px;
+  background-color: #f0f0f0;
+}
+  </style>
+  
