4b95f27f2f76cb09c7ea8f69d7c6d45ab57776b0
appointat <appointat@shu.edu.cn>
2025-03-10 15:40:37 +0800

    refactor(session): improve job submission by handling missing session IDs and ensuring chat history context is retrieved correctly

--- app/core/sdk/wrapper/session_wrapper.py
+++ app/core/sdk/wrapper/session_wrapper.py
@@ -1,8 +1,8 @@
-from typing import Optional
+from typing import List, Optional
 
 from app.core.common.async_func import run_in_thread
 from app.core.model.job import Job
-from app.core.model.message import ChatMessage
+from app.core.model.message import ChatMessage, TextMessage
 from app.core.model.session import Session
 from app.core.sdk.wrapper.job_wrapper import JobWrapper
 from app.core.service.message_service import MessageService
@@ -19,10 +19,14 @@ class SessionWrapper:
     def submit(self, message: ChatMessage) -> JobWrapper:
         """Submit the job."""
         # get chat history (text messages), and it will be used as the context of the job
-        message_service: MessageService = MessageService.instance
-        history_text_messages = message_service.filter_text_messages_by_session(
-            session_id=message.get_session_id()
-        )
+        session_id: Optional[str] = message.get_session_id()
+        if session_id:
+            message_service: MessageService = MessageService.instance
+            history_text_messages: List[TextMessage] = (
+                message_service.filter_text_messages_by_session(session_id=session_id)
+            )
+        else:
+            history_text_messages = []
 
         job = Job(
             goal=message.get_payload(),
