45b5289b81a580a33494cced659e01fd73f23477
gengsheng.wp <gengsheng.wp@antgroup.com>
2025-03-19 21:45:45 +0800

    feat: 语言切换样式调整

--- web/src/components/Language/index.tsx
+++ web/src/components/Language/index.tsx
@@ -1,32 +1,36 @@
 import { getLocale, useSearchParams } from 'umi';
 import styles from './index.less'
+import { Button, Tooltip } from 'antd';
 
 
 const Language: React.FC = () => {
-    let [searchParams, setSearchParams] = useSearchParams();
-    const locale = getLocale()
+  let [searchParams, setSearchParams] = useSearchParams();
+  const locale = getLocale()
 
 
-    const onChangeLang = () => {
+  const onChangeLang = () => {
 
-        const params = Object.fromEntries(searchParams.entries());
-        const newParams = { ...params };
+    const params = Object.fromEntries(searchParams.entries());
+    const newParams = { ...params };
 
-        if (locale === 'en-US') {
-            // 如果当前是英文，删除lang参数（切换到中文）
-            delete newParams.lang;
-        } else {
-            // 如果当前是中文，设置为英文
-            newParams.lang = 'en';
-        }
-
-        setSearchParams(newParams);
+    if (locale === 'en-US') {
+      // 如果当前是英文，删除lang参数（切换到中文）
+      delete newParams.lang;
+    } else {
+      // 如果当前是中文，设置为英文
+      newParams.lang = 'en';
     }
-    return <div className={styles.language} style={{
 
-    }} onClick={onChangeLang}>
-        {locale === 'en-US' ? 'EN' : '中'}
-    </div>
+    setSearchParams(newParams);
+  }
+  return <Tooltip title='English/中文'>
+    <Button
+      className={styles.language}
+      type='text'
+      size='small'
+      onClick={onChangeLang}>{locale === 'en-US' ? 'EN' : '中'}
+    </Button>
+  </Tooltip>
 };
 
 export default Language;
