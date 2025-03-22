1823980a4b04b440cd3026434d8d2cee16352a57
秉翟 <lyusonglin.lsl@antgroup.com>
2025-03-22 16:02:23 +0800

    fix typo

--- app/core/prompt/knowledge.py
+++ app/core/prompt/knowledge.py
@@ -1,7 +1,7 @@
 KNOWLEDGE_PROMPT = """
 All related knowledges retrieved from the knowledge base are listed below.
 
-[Knowledges From Gloabal Knowledge Base]:
+[Knowledges From Global Knowledge Base]:
 {global_knowledges}
 
 [Knowledges From Local Knowledge Base]:
