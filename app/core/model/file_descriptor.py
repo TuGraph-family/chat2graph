04d9df439552a7312edeef32b5428148f6f17c75
秉翟 <lyusonglin.lsl@antgroup.com>
2025-04-17 11:25:44 +0800

    fix multiple files in one md5 folder and change path to the file path instead of folder path

--- app/core/model/file_descriptor.py
+++ app/core/model/file_descriptor.py
@@ -20,8 +20,7 @@ class FileDescriptor:
     def get_payload(self) -> str:
         """Get the content of the file."""
         if self.path:
-            file_name = os.listdir(self.path)[0]
-            file_path = os.path.join(self.path, file_name)
+            file_path = self.path
             with open(file_path, encoding="utf-8") as f:
                 return f.read()
         else:
