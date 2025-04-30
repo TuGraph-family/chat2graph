c96cb3336ebcb1f5cafc5000b8fa4f13e007b915
AI-J-IN <95836831+AI-J-IN@users.noreply.github.com>
2025-04-30 14:13:08 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+cd "$(dirname "$(readlink -f "$0")")" &> /dev/null && source utils.sh || exit
+
+# pre-check
+pids=$(get_pids)
+if [[ -n $pids ]]; then
+  fatal "Chat2Graph server already started (pid: $pids)"
+fi
+
+# prepare log path
+mkdir -p "$(dirname ${SERVER_LOG_PATH})"
+   
+# create a new log file with a timestamp
+timestamp=$(date +"%Y%m%d_%H%M%S")
+new_log_file="$(dirname ${SERVER_LOG_PATH})/server_${timestamp}.log"
+ln -sf "$new_log_file" "${SERVER_LOG_PATH}"  # Update the symbolic link to point to the new log file
+
+# startup python server
+project_root=$(dirname "$(pwd)")
+cd ${project_root} || exit 1
+nohup python ${project_root}/app/server/bootstrap.py >> "${new_log_file}" 2>&1 </dev/null &
+
+# print startup logs
+tail -f "${new_log_file}" | while IFS= read -r line
+do
+  if [[ "$line" == *"Press CTRL+C to quit"* ]]; then
+      pkill -P $$ tail
+      echo "Detail logs in ${new_log_file}"
+  else
+    echo "$line"
+  fi
+done
+
+# post-check
+pids=$(get_pids)
+if [[ -n $pids ]]; then
+  info "Chat2Graph server started success ! (pid: $pids)"
+else
+  fatal "Chat2Graph server started failed, logs in ${new_log_file}"
+fi
